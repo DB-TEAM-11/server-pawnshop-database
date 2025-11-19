9601757c20994ccc9164af37fe17a1334f7b202e
minty <0909minty@gmail.com>
2025-11-19 22:23:22 +0900

    게임세션 키 인서트 쿼리문 수정

--- src/phase3/queries/InsertGameSession.java
+++ src/phase3/queries/InsertGameSession.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class InsertGameSession {
-    private static final String INSERT_QUERY = "INSERT INTO GAME_SESSION (PLAYER_KEY, NICKNAME, SHOP_NAME) VALUES ((SELECT PLAYER_KEY FROM PLAYER WHERE SESSION_TOKEN = '%s'), '%s', '%s')";
-
+    private static final String INSERT_QUERY = "INSERT INTO GAME_SESSION (PLAYER_KEY, NICKNAME, SHOP_NAME, UNLOCKED_SHOWCASE_COUNT) VALUES ((SELECT PLAYER_KEY FROM PLAYER WHERE SESSION_TOKEN = '%s'), '%s', '%s', 8)";
+    
     public static void insertGameSession(Connection connection, String sessionToken, String nickname, String shopName) throws SQLException {
         Statement statement = connection.createStatement();
         statement.executeUpdate(String.format(INSERT_QUERY, sessionToken, nickname, shopName));
